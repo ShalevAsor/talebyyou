791ceea2352f4d7010708e4e3de39f075b3bc975
ShalevAsor <shalevasor@gmail.com>
2025-05-26 11:21:11 +0300

    Update prisma client

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,13 +1,30 @@
-import { PrismaClient } from "../generated/prisma";
-import { withAccelerate } from "@prisma/extension-accelerate";
+// import { PrismaClient } from "../generated/prisma";
+// import { withAccelerate } from "@prisma/extension-accelerate";
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient;
+// const globalForPrisma = global as unknown as {
+//   prisma: PrismaClient;
+// };
+
+// const prisma =
+//   globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
+
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+// export default prisma;
+import { PrismaClient } from "@prisma/client";
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
 };
 
 const prisma =
-  globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
+  });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
 
 export default prisma;
